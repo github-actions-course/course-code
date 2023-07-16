0bc97bbcc6fde56b16a3c4d24637517d045c4801
alialaa <hello@alialaa.com>
2023-07-16 11:44:19 +0300

    test failing

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  throw new Error("Some error message");
   core.debug("Debug Message");
   core.warning("Warning message");
   core.error("Error message");
